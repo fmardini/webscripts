5da08544ce92dbb165c9a297ec3cb63dcc60219d
Fouad Mardini <f.mardini@gmail.com>
2011-04-29 21:32:00 +0300

    cleanup

--- evene.fr.rb
+++ evene.fr.rb
@@ -6,22 +6,14 @@ require 'sanitize'
 
 base_url = 'http://www.evene.fr/citations/auteur.php?ida=40'
 
-doc = Hpricot(open(base_url).read)
+doc   = Hpricot(open(base_url).read)
 pages = doc.search("html body div div.page div#left.content table:last tr td.B13 a.B13")
-urls = {}
-pages.each { |page| urls[Addressable::URI.join(base_url, page[:href])] = true }
-
-def get_quotes(doc)
-  quotes = doc.search("html body div div.page div#left.content table:last tr td span.N12.txtNr")
-  qs = []
-  quotes.each { |q| qs << Sanitize.clean(q.inner_html) }
-  qs
-end
+urls  = pages.inject({}) { |h, page| h[Addressable::URI.join(base_url, page[:href])] = true; h }
 
 File.open('quotes.txt', 'w') do |f|
   [doc].concat(urls.keys.map { |u| Hpricot(open(u).read) }).each do |d|
-    get_quotes(d).each do |q|
-      f.puts q
+    d.search("html body div div.page div#left.content table:last tr td span.N12.txtNr").each do |q|
+      f.puts Sanitize.clean(q.inner_html)
     end
   end
 end
